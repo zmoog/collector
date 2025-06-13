9ff2999f9c1ce40d602a03dc63ed273f1d59cf0c
Maurizio Branca <maurizio.branca@elastic.co>
2025-06-14 01:29:11 +0200

    minInterval is now 30s
    For making testing easier

--- receiver/zcsazzurroreceiver/config.go
+++ receiver/zcsazzurroreceiver/config.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+const (
+	minInterval = 30 * time.Second
+)
+
 type Config struct {
 	Interval string `mapstructure:"interval"`
 	ClientID string `mapstructure:"client_id"`
@@ -13,15 +17,25 @@ type Config struct {
 }
 
 func (cfg *Config) Validate() error {
-	// Validate that min interval is 5 minutes
+	// Validate that min interval is `minInterval`
 	interval, err := time.ParseDuration(cfg.Interval)
 	if err != nil {
 		return fmt.Errorf("invalid interval: %w", err)
 	}
-	if interval < 5*time.Minute {
+	if interval < minInterval {
 		// ZCS updates data every 5 minutes, so it makes no sense
 		// to have a smaller interval.
-		return fmt.Errorf("interval must be at least 5 minutes")
+		//
+		// However, having a smaller interval comes handy
+		// when testing.
+		//
+		// The receiver checks the last update time
+		// and only processes metrics if the last update
+		// is newer than the last processed time.
+		//
+		// So, having a smaller interval than the update
+		// interval is not a problem.
+		return fmt.Errorf("interval must be at least %s", minInterval)
 	}
 
 	if cfg.AuthKey == "" {
@@ -33,6 +47,5 @@ func (cfg *Config) Validate() error {
 	if cfg.ThingKey == "" {
 		return fmt.Errorf("thing_key is required")
 	}
-
 	return nil
 }
