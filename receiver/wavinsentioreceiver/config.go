d80d062e6297053ee640903301a9313efe824b1f
Maurizio Branca <maurizio.branca@gmail.com>
2025-09-27 17:38:11 +0200

    feat(wavinsentio): use OTel scraper and new WS SDK v2 (#111)
    Two main changes:
    
    - use the go.opentelemetry.io/collector/scraper package
    - use the github.com/zmoog/ws/v2 package
    
    Closes: #107

--- receiver/wavinsentioreceiver/config.go
+++ receiver/wavinsentioreceiver/config.go
@@ -3,30 +3,37 @@ package wavinsentioreceiver
 import (
 	"fmt"
 	"time"
+
+	"go.opentelemetry.io/collector/scraper/scraperhelper"
+)
+
+const (
+	MinCollectionInterval = 60 * time.Second
 )
 
 type Config struct {
-	Interval string `mapstructure:"interval"`
-	Username string `mapstructure:"username"`
-	Password string `mapstructure:"password"`
+	scraperhelper.ControllerConfig `mapstructure:",squash"`
+	Endpoint                       string `mapstructure:"endpoint"`
+	Username                       string `mapstructure:"username"`
+	Password                       string `mapstructure:"password"`
+	WebApiKey                      string `mapstructure:"web_api_key"`
 }
 
 func (cfg Config) Validate() error {
-	interval, err := time.ParseDuration(cfg.Interval)
-	if err != nil {
-		return fmt.Errorf("invalid interval: %w", err)
+	if cfg.CollectionInterval < MinCollectionInterval {
+		return fmt.Errorf("interval must be at least %s", MinCollectionInterval)
 	}
-	if interval < 1*time.Minute {
-		return fmt.Errorf("interval must be at least 1 minute")
+	if cfg.Endpoint == "" {
+		return fmt.Errorf("endpoint is required")
 	}
-
 	if cfg.Username == "" {
 		return fmt.Errorf("username is required")
 	}
-
 	if cfg.Password == "" {
 		return fmt.Errorf("password is required")
 	}
-
+	if cfg.WebApiKey == "" {
+		return fmt.Errorf("web_api_key is required")
+	}
 	return nil
 }
